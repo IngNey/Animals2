c66cc4401dd0f2fccb9b28083342477d0e82285a
VICTOR MANUEL MONDRAGON M <v_mondrag@hotmail.com>
2023-11-13 18:45:01 -0500

    Add files via upload

--- settings.py
+++ settings.py
@@ -30,8 +30,11 @@ DEFAULT_DETECT_IMAGE = IMAGES_DIR / 'office_4_detected.jpg'
 # Videos config
 VIDEO_DIR = ROOT / 'videos'
 VIDEO_1_PATH = VIDEO_DIR / 'video_1.mp4'
+
+
 VIDEOS_DICT = {
     'video_1': VIDEO_1_PATH
+
 }
 
 # ML Model config
--- video_object_detection.py
+++ video_object_detection.py
@@ -16,10 +16,11 @@ st.set_page_config(
 # Creating sidebar
 with st.sidebar:
     st.header("Image/Video Config")     # Adding header to sidebar
+
     # Adding file uploader to sidebar for selecting videos
     source_vid = st.sidebar.selectbox(
         "Choose a video...",
-        ["videos/video_1.mp4"])
+        ["videos/video_1.mp4", "videos/video_2.mp4"])  # Actualizar lista de selección
 
     # Model Options
     confidence = float(st.slider(
@@ -34,18 +35,16 @@ except Exception as ex:
     st.error(
         f"Unable to load model. Check the specified path: {model_path}")
     st.error(ex)
-# st.write("Model loaded successfully!")
 
 if source_vid is not None:
-    with open(str(source_vid), 'rb') as video_file:
+    with open(source_vid, 'rb') as video_file:
         video_bytes = video_file.read()
     if video_bytes:
         st.video(video_bytes)
     if st.sidebar.button('Detect Objects'):
-        vid_cap = cv2.VideoCapture(
-            "videos/video_1.mp4")
+        vid_cap = cv2.VideoCapture(source_vid)  # Usar la variable source_vid
         st_frame = st.empty()
-        while (vid_cap.isOpened()):
+        while vid_cap.isOpened():
             success, image = vid_cap.read()
             if success:
                 image = cv2.resize(image, (720, int(720*(9/16))))
